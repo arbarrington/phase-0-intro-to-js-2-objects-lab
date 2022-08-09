6d4c45e6d200a1a88ea3ace24f4036cc3236734a
Austin Barrington <arbarrington12@gmail.com>
2022-08-09 17:41:29 -0600

    commit tree 625cc82ec930219063231988c3183e893b6ac509

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {name: "Austin", streetAddress: "Main Street"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key]=value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee.streetAddress = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,name) {
+    const newObj = {...employee};
+    delete newObj.name;
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name; 
+    return employee;
+}
